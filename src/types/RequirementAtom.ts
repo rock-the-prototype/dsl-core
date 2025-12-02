39c04cc34483bf14359e406c0619a1eca6e6c421
Sascha Block <block@inztitut.de>
2025-12-02 20:41:59 +0100

    Add RequirementAtom which represents a structure from our DSL.

---
+++ src/types/RequirementAtom.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Sascha Block
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+export interface RequirementAtom {
+    id: string;
+    title?: string;
+    description?: string;
+    source?: string;
+    version?: string;
+
+    // Optional payload depending on parser phase
+    raw?: string;
+    normalized?: string;
+}
