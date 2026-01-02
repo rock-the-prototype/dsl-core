d6523d9d603cdd7bbc533b85af53140f208c3a0d
Sascha Block <58812591+Sascha-Block@users.noreply.github.com>
2026-01-02 19:11:09 +0100

    Add generateReport and DslCoreReport exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,5 @@
 // src/mod.ts
 export type { StatementValidation, FileValidation } from "./api/public_api.ts";
 export { validateText, validateFile, validatePath } from "./api/public_api.ts";
+export { generateReport } from "./api/public_api.ts";
+export type { DslCoreReport } from "./report/buildReport.ts";
