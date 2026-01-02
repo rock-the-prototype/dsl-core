e3184a19e706a83e5d40a59ee7d07208be6218a8
Sascha Block <58812591+Sascha-Block@users.noreply.github.com>
2026-01-02 19:13:52 +0100

    Add cmdReport function to generate reports

---
+++ cli/src/report.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2026 Sascha Block
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+
+import { generateReport } from "../../../src/mod.ts";
+
+export async function cmdReport(
+  target: string,
+  opts: { pretty: boolean; out?: string },
+) {
+  // MVP: Engine version does not (yet) come automatically -> can be read later from git tag / deno.json.
+  const report = await generateReport(target, { engineVersion: "0.0.0-dev" });
+
+  const json = JSON.stringify(report, null, opts.pretty ? 2 : 0);
+
+  if (opts.out) {
+    await Deno.writeTextFile(opts.out, json);
+    console.log(`Wrote report to ${opts.out}`);
+    return;
+  }
+
+  console.log(json);
+}
