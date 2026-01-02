eedf058c2200564087fbb7b0575a098333d00859
Sascha Block <58812591+Sascha-Block@users.noreply.github.com>
2026-01-02 17:46:34 +0100

    Add report command to CLI
    Update: report

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,10 +1,20 @@
+/*
+ * Copyright 2026 Sascha Block
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 import { parseArgs } from "jsr:@std/cli/parse-args";
 import { cmdValidate } from "./commands/validate.ts";
 import { cmdInspect } from "./commands/inspect.ts";
+import { cmdReport } from "./commands/report.ts";
 
 const args = parseArgs(Deno.args, {
   boolean: ["json", "pretty"],
-  alias: { j: "json", p: "pretty" },
+  string: ["out"],
+  alias: { j: "json", p: "pretty", o: "out" },
 });
 
 const [command, ...rest] = args._.map(String);
@@ -12,8 +22,9 @@ const target = rest.join(" ").trim();
 
 if (!command) {
   console.error(`Usage:
-  dsl inspect ["DSL statement"]     # or via STDIN
-  dsl validate <file|dir> [--json] [--pretty]`);
+  dsl inspect ["DSL statement"]             # or via STDIN
+  dsl validate <file|dir> [--json] [--pretty]
+  dsl report <file|dir> [--pretty] [--out report.json]`);
   Deno.exit(2);
 }
 
@@ -28,6 +39,14 @@ switch (command) {
       Deno.exit(2);
     }
     await cmdValidate(target, { json: !!args.json, pretty: !!args.pretty });
+    break;
+
+  case "report":
+    if (!target) {
+      console.error("Missing <file|dir>.");
+      Deno.exit(2);
+    }
+    await cmdReport(target, { pretty: !!args.pretty, out: args.out ? String(args.out) : undefined });
     break;
 
   default:
